59a9684eb8dd1d225e9f367326897b50e1992d13
isma-91 <isma91@hotmail.it>
2023-02-02 15:28:29 +0100

    Add api/posts routes & api/PostController

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function show(Post $post)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/posts', 'Api\Postcontroller@index')->name('posts.index');
+Route::get('/posts/{post}', 'Api\Postcontroller@show')->name('posts.show');
+/*
+tutte queste rotte in api.php mette in automatico al percorso "api" come prima voce, quindi queste 2 rotte appena create stanno per:
+api/posts e api/posts/{post}
+*/
